7ac68ddcca662ef5ebd69047782a59026208472b
adiko01 <dev@adiko01.de>
2023-10-20 08:18:10 +0200

    MC-Plugin Base

---
+++ src/main/java/de/adiko01/mcds/MC_DS.java
@@ -0,0 +1,17 @@
+package de.adiko01.mcds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MC_DS extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
