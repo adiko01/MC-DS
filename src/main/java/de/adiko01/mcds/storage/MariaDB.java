7e5d99b8897cf0c0c2bfc67497ada134fabb261c
adiko01 <dev@adiko01.de>
2023-10-20 14:51:41 +0200

    Erstelle Tabellen und Service für den Server

--- src/main/java/de/adiko01/mcds/storage/MariaDB.java
+++ src/main/java/de/adiko01/mcds/storage/MariaDB.java
@@ -3,6 +3,7 @@ package de.adiko01.mcds.storage;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**Das Objekt f&uuml;r eine MariaDB verbindung
  * @author adiko01
@@ -14,37 +15,37 @@ public class MariaDB extends Storage{
      * Der Host
      * @since 1.0
      */
-    private String Host;
+    private final String Host;
 
     /**
      * Der Port
      * @since 1.0
      */
-    private int Port;
+    private final int Port;
 
     /**
      * Die Datenbank
      * @since 1.0
      */
-    private String Database;
+    private final String Database;
 
     /**
      * Der Username
      * @since 1.0
      */
-    private String Username;
+    private final String Username;
 
     /**
      * Das Password
      * @since 1.0
      */
-    private String Password;
+    private final String Password;
 
     /**
      * Der Tabellenprefix
      * @since 1.0
      */
-    private String Prefix;
+    private final String Prefix;
 
     /**
      * Die Verbindung zur Datenbank
@@ -72,7 +73,6 @@ public class MariaDB extends Storage{
     }
 
     public boolean createConnection() {
-
         try {
             if (conn != null && !conn.isClosed()) {
                 return true; // Verbindung besteht bereits
@@ -87,10 +87,57 @@ public class MariaDB extends Storage{
             // Stelle die Verbindung zur Datenbank her
             conn = DriverManager.getConnection(url, Username, Password);
 
-            System.out.println("Verbindung zur Datenbank hergestellt!");
+
+            //Erstelle die Tabelle Services
+            String SQL = "CREATE TABLE IF NOT EXISTS `" + Prefix + "Services` (" +
+                    "  `ID` int(11) NOT NULL AUTO_INCREMENT," +
+                    "  `Name` text NOT NULL," +
+                    "  `URL` text DEFAULT NULL," +
+                    "  `Created` timestamp NULL DEFAULT current_timestamp()," +
+                    "  PRIMARY KEY (`ID`)" +
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+            sendData(SQL);
+
+            //Erstellen der Tabelle Users
+             SQL = "CREATE TABLE IF NOT EXISTS `" + Prefix + "Users` (" +
+                    "  `UUID` tinytext NOT NULL," +
+                    "  `Username` tinytext NOT NULL," +
+                    "  `RegisterTime` timestamp NOT NULL DEFAULT current_timestamp()," +
+                    "  `PwChangeTime` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()," +
+                    "  `PwChangeService` int(11) NOT NULL," +
+                    "  `PasswortSHA256` text NOT NULL," +
+                    "  PRIMARY KEY (`UUID`(36))," +
+                    "  KEY `FK_Users_Services` (`PwChangeService`)," +
+                    "  CONSTRAINT `FK_Users_Services` FOREIGN KEY (`PwChangeService`) REFERENCES `mcds_Services` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION" +
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+            sendData(SQL);
+
+            //Erstelle den Eintrag der Services für das Plugin auf dem MC Server
+            SQL = "INSERT INTO `" + Prefix + "Services` (`ID`, `Name`) SELECT 1, 'MC-DS Plugin Minecraft' WHERE NOT EXISTS (SELECT 1 FROM `" + Prefix + "Services` WHERE id = 1);";
+            sendData(SQL);
+
             return true;
         } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Führe ein SQL Statement aus
+     * @param Query Das SQL-Query
+     * @return {@code true}, wenn erfolgreich
+     */
+    private boolean sendData(String Query) {
+
+        try {
+            if (conn == null || conn.isClosed()) {
+                createConnection();
+            }
+            Statement statement = conn.createStatement();
+            statement.executeUpdate(Query);
+            statement.close();
+            return true;
+        } catch (SQLException e) {
             return false;
         }
     }
