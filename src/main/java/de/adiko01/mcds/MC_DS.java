27fbc09c327361f956acd2d3c974a959a4b23751
adiko01 <dev@adiko01.de>
2023-10-20 10:31:55 +0200

    Plugin Meldet ein und ausschalten

--- src/main/java/de/adiko01/mcds/MC_DS.java
+++ src/main/java/de/adiko01/mcds/MC_DS.java
@@ -1,17 +1,34 @@
 package de.adiko01.mcds;
 
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Main Class
+ * @author adiko01
+ * @version 1.0
+ */
 public final class MC_DS extends JavaPlugin {
+
+    /** Die Pluginbeschreibungs-Datei
+     * @since 1.0
+     **/
+    PluginDescriptionFile pdf;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        PluginDescriptionFile pdf = this.getDescription();
 
+
+
+        getLogger().info("MC-DS " + pdf.getVersion() + " is enabled.");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("MC-DS is disabled.");
     }
+
 }
